7a2e2090017d422740b998d0bcec14ef5424e5a2
Ildar Gafurov <dev@binblob.com>
2016-09-08 18:33:36 +0300

    wasp_launcher/launcher_registry.py: added tested, documented code

---
+++ tests/wasp_launcher_launcher_registry_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from wasp_launcher.launcher_registry import WLauncherRegistry, WLauncherTask
+
+from wasp_general.task.dependency import WTaskDependencyRegistry, WTaskDependencyRegistryStorage, WDependentTask
+from wasp_general.task.sync import WSyncTask
+
+
+class TestWLauncherRegistry:
+
+	def test_implementation(self):
+		assert(isinstance(WLauncherRegistry(), WTaskDependencyRegistry) is True)
+		assert(isinstance(WLauncherRegistry.__registry_storage__, WTaskDependencyRegistryStorage) is True)
+
+
+class TestWLauncherTask:
+
+	def test_implementation(self):
+		with pytest.raises(TypeError):
+			WLauncherTask()
+
+		assert(issubclass(WLauncherTask, WSyncTask) is True)
+		assert(isinstance(WLauncherTask, WDependentTask) is True)
+		assert(WLauncherTask.__registry__ == WLauncherRegistry)
---
+++ wasp_launcher/launcher_registry.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# wasp_launcher/launcher_registry.py
+#
+# Copyright (C) 2016 the wasp-launcher authors and contributors
+# <see AUTHORS file>
+#
+# This file is part of wasp-launcher.
+#
+# Wasp-launcher is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Wasp-launcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with wasp-launcher.  If not, see <http://www.gnu.org/licenses/>.
+
+# noinspection PyUnresolvedReferences
+from wasp_launcher.version import __author__, __version__, __credits__, __license__, __copyright__, __email__
+# noinspection PyUnresolvedReferences
+from wasp_launcher.version import __status__
+
+from wasp_general.task.dependency import WTaskDependencyRegistry, WTaskDependencyRegistryStorage, WDependentTask
+from wasp_general.task.sync import WSyncTask
+
+
+class WLauncherRegistry(WTaskDependencyRegistry):
+	""" Main registry to keep tasks responded to the launcher starting
+	"""
+	__registry_storage__ = WTaskDependencyRegistryStorage()
+	""" Default storage for this type of registry
+	"""
+
+
+class WLauncherTask(WSyncTask, metaclass=WDependentTask):
+	""" Basic class for derived classes, that does real work in launcher starting process
+	"""
+
+	__registry__ = WLauncherRegistry
+	""" Link to registry
+	"""
+
+	__registry_tag__ = '::wasp_launcher'
+	""" Default tag (just because WDependentTask needs it)
+	"""
