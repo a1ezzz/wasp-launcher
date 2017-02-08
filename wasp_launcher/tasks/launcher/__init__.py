98ad09921148d9d524f672fc0b617d56e2699921
Ildar Gafurov <dev@binblob.com>
2017-02-08 19:05:44 +0300

    wasp_launcher/tasks/launcher/__init__.py: tasks updated

--- wasp_launcher/tasks/launcher/__init__.py
+++ wasp_launcher/tasks/launcher/__init__.py
@@ -4,4 +4,7 @@ import wasp_launcher.tasks.launcher.registry
 import wasp_launcher.tasks.launcher.log
 import wasp_launcher.tasks.launcher.config
 import wasp_launcher.tasks.launcher.app_loader
+import wasp_launcher.tasks.launcher.web_service
+import wasp_launcher.tasks.launcher.web_debugger
+import wasp_launcher.tasks.launcher.templates
 import wasp_launcher.tasks.launcher.broker
