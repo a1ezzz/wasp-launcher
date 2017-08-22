d7ddbdc1c8a2eb7d3bc2361bd0a18c93c83daa45
Ildar Gafurov <dev@binblob.com>
2017-08-22 17:43:54 +0300

    wasp_launcher/guest_apps/commands.py: removed due to the following broker update

--- wasp_launcher/guest_apps/__init__.py
+++ wasp_launcher/guest_apps/__init__.py
@@ -1,8 +1,7 @@
 
 from wasp_launcher.guest_apps.wasp import WWaspBasicApps
 from wasp_launcher.guest_apps.debug import WWaspDebugApps
-from wasp_launcher.guest_apps.commands import WWaspBrokerCommands
 
 
 def __wasp_launcher_apps__():
-	return [WWaspBasicApps, WWaspDebugApps, WWaspBrokerCommands]
+	return [WWaspBasicApps, WWaspDebugApps]
