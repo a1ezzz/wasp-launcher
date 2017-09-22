aab26d709b703308b256933fea525aa57748fd30
Ildar Gafurov <dev@binblob.com>
2017-09-22 17:35:26 +0300

    debian packaging files added

--- wasp_launcher/guest_apps/__init__.py
+++ wasp_launcher/guest_apps/__init__.py
@@ -1,7 +1,7 @@
 
-from wasp_launcher.guest_apps.wasp import WWaspBasicApps
-from wasp_launcher.guest_apps.debug import WWaspDebugApps
-
 
 def __wasp_launcher_apps__():
+	from wasp_launcher.guest_apps.wasp import WWaspBasicApps
+	from wasp_launcher.guest_apps.debug import WWaspDebugApps
+
 	return [WWaspBasicApps, WWaspDebugApps]
